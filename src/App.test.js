221dbe39d7f47ace83e51dc6c521fd389d42f08d
Tita Wijayanti <wijayantitita094@gmail.com>
2021-12-02 10:30:46 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('correct react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.href).toContain("https://reactjs.org");
+});
